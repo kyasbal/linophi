a9a963980fb52159eeef718d584651dd1e71cff5
LimeStreem <LimeStreem@gmail.com>
2014-08-30 01:46:18 +0900

    アカウントのコントローラーにはhttp:/ドメイン/Account/~でアクセスできるようにした。

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Web
@@ -12,21 +8,15 @@ namespace Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-name: "Edit",
-url: "Edit",
-defaults: new { controller = "Edit", action = "Index", id = UrlParameter.Optional }
-);
-            routes.MapRoute(
-                name: "Default",
-                url: "{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-            routes.MapRoute(
-    name: "Pages",
-    url: "Pages/{controller}/{action}/{id}",
-    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-);
+            routes.MapRoute("Account", "Account/{action}", new { controller = "Account" }
+    );
+            routes.MapRoute("Edit", "Edit", new {controller = "Edit", action = "Index", id = UrlParameter.Optional}
+                );
+            routes.MapRoute("Default", "{id}", new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
+            routes.MapRoute("Pages", "Pages/{controller}/{action}/{id}",
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
