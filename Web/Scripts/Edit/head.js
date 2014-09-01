537f93e25cae4bcaa86badbbf2544404bec13468
coil398 <beep.kawase@gmail.com>
2014-09-02 03:37:15 +0900

    ドロップダウンわかんね！！！！！！！ TSとLESSは用意したので誰か頼んだ・・・。

---
+++ Web/Scripts/Edit/head.js
@@ -0,0 +1,14 @@
+﻿$('.menu').on({
+    click: function (e) {
+        e.preventDefault();
+
+        var cls = $(".dropdown-menu").attr("class");
+
+        if (cls.match(/open/)) {
+            $(".dropdown-menu").removeClass("open");
+        } else {
+            $(".dropdown-menu").addClass("open");
+        }
+    }
+});
+//# sourceMappingURL=head.js.map
---
+++ Web/Scripts/Edit/head.ts
@@ -0,0 +1,17 @@
+﻿$('.menu').on({
+    click: function(e)
+    {
+        e.preventDefault();
+
+        var cls = $(".dropdown-menu").attr("class");
+
+        if (cls.match(/open/))
+        {
+            $(".dropdown-menu").removeClass("open");
+        }
+        else
+        {
+            $(".dropdown-menu").addClass("open");
+        }
+    }
+});
