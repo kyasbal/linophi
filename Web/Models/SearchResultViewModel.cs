3effc0eebf9f78243be05131115cf1fbd69aa2f2
LimeStreem <LimeStreem@gmail.com>
2014-09-02 22:41:30 +0900

    検索画面のベースの作成

---
+++ Web/Models/SearchResultViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class SearchResultViewModel
+    {
+        public SearchResultArticle[] Articles { get; set; }
+    }
+
+    public class SearchResultArticle
+    {
+        public string Title { get; set; }
+
+        public int PageView { get; set; }
+
+        public int LabelCount { get; set; }
+
+        public string ArticleId { get; set; }
+    }
+}
