37ed81679ca353a1aa54324c8a6592231f7dfd97
LimeStreem <LimeStreem@gmail.com>
2014-09-04 05:37:18 +0900

    画像アップロードAPIの準備

---
+++ Web/Api/Content/ContentUploadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Web.Storage;
+using Web.Storage.Connection;
+using Web.Utility;
+
+namespace Web.Api.Content
+{
+    public class ContentUploadController : Controller
+    {
+        public ActionResult UploadFromExternal(string url)
+        {
+            ImageUploaderManager manager=new ImageUploaderManager(new BlobStorageConnection());
+            string contentType = "";
+            var st=manager.AddUrlResource(url,out contentType);
+            return new ImageResult(st, contentType);
+        }
+    }
+}
