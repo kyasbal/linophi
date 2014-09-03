3ac8ad5ee1f918488a4ed60739a0d6e10c5ddc18
limestreem@gmail.com <limestreem@gmail.com>
2014-09-03 15:09:08 +0900

    新貼り付け機能途中経過

--- Web/Scripts/Home/PostitPaster.js
+++ Web/Scripts/Home/PostitPaster.js
@@ -1,16 +1,107 @@
 ﻿$(function () {
+    var htmlHeight = $('.foot').offset().top + $('.foot').outerHeight();
+
+    $('html').css({
+        "height": htmlHeight + "px"
+    });
+
     /*
     * ふせんをクリックされたら左側に影的なものを出して周りを暗くする
     * 次のタイミングにクリックされたら貼り付ける
     * 貼り付けられた（クリック領域でクリックされたら）そこに貼る
     * それはこっちで用意した場所に追加されるが、すでに貼られていた場合は値を増やす。
     */
-    $('.postit-list > img').click(function () {
-        $('.fade-layer').css({
+    var pasteMode = false;
+
+    var $fadeLayer = $('.fade-layer');
+
+    var src;
+    var dropboxPos = $('.contentswrapper').offset().top, dropboxHeight = $('.contentswrapper').outerHeight();
+
+    var posX = $('.dropbox').offset().left + 20, posY = dropboxPos + 10;
+
+    $('.article-container > *').each(function (i) {
+        var $ele = $('[class^="x_p-"]:nth-child(' + (i + 1) + ')');
+
+        var className = $ele.attr("class");
+
+        // alert(className);
+        var eleHeight = $ele.outerHeight(), elePos = $ele.offset().top;
+
+        $('.dropbox').append('<div class="' + className + '"></div>');
+
+        $('.dropbox > .' + className).css({
+            "position": "absolute",
+            "top": elePos + "px",
+            "height": eleHeight + "px",
+            "width": "300px"
+        });
+    });
+
+    $('.postit-list > img').click(function (event) {
+        pasteMode = true;
+
+        $fadeLayer.css({
             "visibility": "visible",
             "opacity": 0.5
         });
+
+        src = event.currentTarget.src; // なぜかVSで赤線がでるけどちゃんと動きます
+        $fadeLayer.html('<img src="' + src + '">');
+
+        $fadeLayer.mousemove(function (e) {
+            if (dropboxPos <= e.pageY && e.pageY <= dropboxPos + dropboxHeight) {
+                posY = e.pageY - 20;
+            }
+            $('.fade-layer > img').css({
+                "position": "absolute",
+                "top": posY + "px",
+                "left": posX + "px"
+            });
+
+            var pHeights = dropboxPos;
+
+            $('.dropbox > [class^="x_p-"]').each(function (i) {
+                var $target = $('.dropbox > [class^="x_p-"]:nth-child(' + (i + 1) + ')');
+                var pHeight = $target.outerHeight();
+                var bg = "none";
+                if (pHeights <= posY && posY <= pHeights + pHeight && pasteMode) {
+                    bg = "#fcc";
+                }
+
+                $target.css({
+                    "background": bg
+                });
+
+                pHeights += pHeight;
+                //console.log($target.attr("class"), pHeight, pHeights, posY, src);
+            });
+        });
+
         console.log("called");
+    });
+
+    $fadeLayer.click(function () {
+        pasteMode = false;
+
+        $fadeLayer.css("opacity", 0);
+        setTimeout(function () {
+            $fadeLayer.css("visibility", "hidden");
+        }, 500);
+
+        var pHeights = dropboxPos;
+
+        $('.dropbox > [class^="x_p-"]').each(function (i) {
+            var $target = $('.dropbox > [class^="x_p-"]:nth-child(' + (i + 1) + ')');
+            var pHeight = $target.outerHeight();
+            if (pHeights <= posY && posY <= pHeights + pHeight) {
+                $target.append('<img src="' + src + '">');
+            }
+
+            pHeights += pHeight;
+
+            console.log($target.attr("class"), pHeight, pHeights, posY, src);
+        });
     });
 });
 /*
--- Web/Scripts/Home/PostitPaster.ts
+++ Web/Scripts/Home/PostitPaster.ts
@@ -1,20 +1,124 @@
 ﻿$(() =>
 {
+
+    var htmlHeight = $('.foot').offset().top + $('.foot').outerHeight();
+
+    $('html').css({
+        "height": htmlHeight + "px"
+    });
+
     /*
      * ふせんをクリックされたら左側に影的なものを出して周りを暗くする
      * 次のタイミングにクリックされたら貼り付ける
      * 貼り付けられた（クリック領域でクリックされたら）そこに貼る
      * それはこっちで用意した場所に追加されるが、すでに貼られていた場合は値を増やす。
      */
-    
 
-    $('.postit-list > img').click(() =>
+    var pasteMode: boolean = false;
+
+    var $fadeLayer: JQuery = $('.fade-layer');
+
+    var src: string;
+    var dropboxPos: number = $('.contentswrapper').offset().top,
+        dropboxHeight: number = $('.contentswrapper').outerHeight();
+
+    var posX: number = $('.dropbox').offset().left + 20,
+        posY: number = dropboxPos + 10;
+
+    $('.article-container > *').each((i) => {
+        var $ele: JQuery = $('[class^="x_p-"]:nth-child(' + (i + 1) + ')');
+
+        var className = $ele.attr("class");
+
+        // alert(className);
+
+        var eleHeight: number = $ele.outerHeight(),
+            elePos: number = $ele.offset().top;
+
+
+        $('.dropbox').append('<div class="' + className + '"></div>');
+
+        $('.dropbox > .' + className).css({
+            "position": "absolute",
+            "top": elePos + "px",
+            "height": eleHeight + "px",
+            "width": "300px",
+        });
+    });
+
+
+    $('.postit-list > img').click((event) =>
     {
-        $('.fade-layer').css({
+        pasteMode = true;
+
+        $fadeLayer.css({
             "visibility": "visible",
             "opacity": 0.5
         });
+
+        src = event.currentTarget.src; // なぜかVSで赤線がでるけどちゃんと動きます
+        $fadeLayer.html('<img src="' + src + '">');
+
+
+
+        $fadeLayer.mousemove((e) =>
+        {
+            if (dropboxPos <= e.pageY && e.pageY <= dropboxPos + dropboxHeight)
+            {
+                posY = e.pageY - 20;
+            }
+            $('.fade-layer > img').css({
+                "position": "absolute",
+                "top": posY + "px",
+                "left": posX + "px"
+            });
+
+            var pHeights: number = dropboxPos;
+
+            $('.dropbox > [class^="x_p-"]').each((i) => {
+                var $target: JQuery = $('.dropbox > [class^="x_p-"]:nth-child(' + (i + 1) + ')');
+                var pHeight: number = $target.outerHeight();
+                var bg = "none";
+                if (pHeights <= posY && posY <= pHeights + pHeight && pasteMode)
+                {
+                    bg = "#fcc";
+                }
+                
+                $target.css({
+                    "background": bg
+                });
+
+                pHeights += pHeight;
+
+                //console.log($target.attr("class"), pHeight, pHeights, posY, src);
+            });
+        });
+
         console.log("called");
+    });
+
+    $fadeLayer.click(() =>
+    {
+        pasteMode = false;
+
+        $fadeLayer.css("opacity", 0);
+        setTimeout(() => {
+            $fadeLayer.css("visibility", "hidden");
+        }, 500);
+
+        var pHeights: number = dropboxPos;
+
+        $('.dropbox > [class^="x_p-"]').each((i) => {
+            var $target: JQuery = $('.dropbox > [class^="x_p-"]:nth-child(' + (i + 1) + ')');
+            var pHeight: number = $target.outerHeight();
+            if (pHeights <= posY && posY <= pHeights + pHeight) {
+                $target.append('<img src="' + src + '">');
+            }
+
+            pHeights += pHeight;
+
+            console.log($target.attr("class"), pHeight, pHeights, posY, src);
+        });
     });
 });
 
