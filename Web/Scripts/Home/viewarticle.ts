0a85fe7c621640096653f447005d0f860062b387
coil398 <beep.kawase@gmail.com>
2014-09-04 21:24:50 +0900

    英数字のみの記事の検索結果を訂正

---
+++ Web/Scripts/Home/viewarticle.ts
@@ -0,0 +1,14 @@
+﻿var top_height;
+
+$(function ()
+ {
+     top_height = $('.top').height();
+});
+
+if(top_height>=200)
+{
+    $(function()
+    {
+        $('.navbar-thumb').css('margin-bottom', '100px');
+    });
+};
