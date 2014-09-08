cbde66fc691b72e27db297ecf697283b878978da
limestreem@gmail.com <limestreem@gmail.com>
2014-09-08 23:03:19 +0900

    まーじ

---
+++ Web/Scripts/Home/CommentIndicator.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+$(function () {
+    var commentJson = JSON.parse($("#comment-info").text());
+
+    $('.article-container > *').each(function (i) {
+        var $ele = $('[class^="x_p-"]:nth-child(' + (i + 1) + ')');
+        var className = $ele.attr("class");
+
+        for (var j = 0, len = commentJson.length; j < len; j++) {
+        }
+
+        $('.widget .' + className).append('<div class="' + className + '-tooltip">76　　　雅　[2014/06/15(日) 19:31:34]<br />個人的には大学かもだけどぶっちゃけどっちも <br / >おもんないｗｗｗｗ <br / >77　　　さち　[2014 / 07 / 10(木) 01:03:45]<br />高校生の頃に戻りたい。 <br />まぢピーターパン症候群。 <br / >大学って楽しいトコだと <br / >思ってたけど <br />大きい大学行かなかった私は負け組。 <br />あ、行かなかったんじゃない。 <br />いけなかったんだ。<br / >78　　　あゆ　[2014 / 08 / 20(水) 11:28:41]<br />断然、高校の方が良かった <br / >79　　　りん　[2014 / 09 / 02(火) 10:02:50]<br />断然、高校！大学の友達は常識ないのばかりだし、性格も悪い。高校に戻りたい。</div>');
+    });
+});
+//# sourceMappingURL=CommentIndicator.js.map
