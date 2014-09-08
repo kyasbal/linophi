dfe89c1dbb6944220a24c3a26a8d3a0abea29396
tsubasa_hayashi <yoku.rin.99@gmail.com>
2014-09-09 07:43:11 +0900

    引用文中で{}一応使えるようにした \{,\}

--- Web/Scripts/Novel/novel-editor.js
+++ Web/Scripts/Novel/novel-editor.js
@@ -898,6 +898,7 @@ var NovelEditer;
             console.warn("link");
             str = str.replace(/&ensp;/g, "\u0006a");
             str = str.replace(/\\{/g, "\u0006b");
+            str = str.replace(/\\}/g, "\u0006c");
             if (str.match(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-_]+(\.jpg|\.jpeg|\.gif|\.png))/g)) {
                 str = str.replace(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-_]+(\.jpg|\.jpeg|\.gif|\.png))/g, "<Img Src=\"/Pages/ContentUpload/UploadFromExternal?url=$1\">");
             }
@@ -906,8 +907,9 @@ var NovelEditer;
             str = str.replace(/\u0006a/g, "&ensp;");
             str = str.replace(/\n/g, "</br>");
             str = str.replace(/(.*)({.*})(.*)/g, "$1$3$2");
-            str = str.replace(/\{(.*?)\}/g, "<p class=\"source\">出典：$1</p>");
+            str = str.replace(/{(.*?)}/g, "<p class=\"source\">出典：$1</p>");
             str = str.replace(/\u0006b/g, "{");
+            str = str.replace(/\u0006c/g, "}");
             return "<blockquote><div class=\"quote\">" + str + "</div></blockquote>";
         };
         return QuotePrefix;
