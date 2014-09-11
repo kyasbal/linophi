83069a1dbb6c947101a38106253b85fb91e5ebf2
LimeStreem <LimeStreem@gmail.com>
2014-09-12 04:32:04 +0900

    いろいろなスタイルの修正、フッターの位置の修正

---
+++ Web/Scripts/Account/Config.ts
@@ -0,0 +1,27 @@
+﻿$(() =>
+{
+    $(window).resize(() => { LoginPage.adjustHeight(); });
+    ConfigPage.adjustHeight();
+    $(".config-submit").click(() =>
+    {
+        $(".config-form").submit();
+    });
+});
+
+module ConfigPage
+{
+    export function getEntireHeight($elem: JQuery): number {
+        return $elem.outerHeight(true);
+    }
+
+    export function adjustHeight()
+    {
+        var header = $("header");
+        var footer = $(".foot");
+        var calcedMargin = $(window).innerHeight() - getEntireHeight(header) - getEntireHeight(footer) - $(".layout").innerHeight();
+        $(".layout").css({
+            'margin-bottom': Math.max(0, calcedMargin / 2),
+            'margin-top': Math.max(0, calcedMargin / 2)
+        });
+    }
+}
