2e871de58553ad47b0183946d09a39b9d6511da0
LimeStreem <LimeStreem@gmail.com>
2014-09-15 23:22:36 +0900

    トピックの編集の実装

--- Web/Controllers/AdminController.cs
+++ Web/Controllers/AdminController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
 using Web.Models;
+using Web.Models.Topic;
 using Web.ViewModel.Admin;
 
 namespace Web.Controllers
@@ -12,17 +14,47 @@ namespace Web.Controllers
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        private ApplicationDbContext dbContext;
+        public ApplicationDbContext DbContext
+        {
+            get
+            {
+                dbContext = dbContext ?? Request.GetOwinContext().Get<ApplicationDbContext>();
+                return dbContext;
+            }
+        }
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult TopicList()
         {
-            var context = Request.GetOwinContext().Get<ApplicationDbContext>();
-            var vm = new TopicListViewModel(context);
-            vm.TopicItems = context.Topics;
+            var vm = new TopicListViewModel(DbContext);
+            vm.TopicItems = DbContext.Topics;
+            return View(vm);
+        }
+        [HttpGet]
+        public async Task<ActionResult> EditTopic(string topicId)
+        {
+            var vm = new EditTopicViewModel();
+            vm.TopicId = topicId;
+            vm.EditTarget = await DbContext.Topics.FindAsync(topicId);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EditTopic(EditTopicViewModel vm)
+        {
+            vm.EditTarget.TopicId = vm.TopicId;
+            TopicModel lastModel = await DbContext.Topics.FindAsync(vm.TopicId);
+            DbContext.Topics.Remove(lastModel);
+            await DbContext.SaveChangesAsync();
+            DbContext.Topics.Add(vm.EditTarget);
+            await DbContext.SaveChangesAsync();
             return View(vm);
         }
     }
