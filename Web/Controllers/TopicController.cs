093696af873f573b10ef640f2decd0883110dcf1
LimeStreem <LimeStreem@gmail.com>
2014-09-17 04:25:51 +0900

    トピック用のページの作成

--- Web/Controllers/TopicController.cs
+++ Web/Controllers/TopicController.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Web.ViewModel.Topic;
 
 namespace Web.Controllers
 {
     public class TopicController : Controller
     {
         // GET: Topic
-        public ActionResult Index(string topicId)
+        public async Task<ActionResult> Index(string topicId)
         {
-            return View();
+            TopicViewModel vm = await TopicViewModel.GenerateViewModel(Request, topicId);
+            if (vm == null)
+            {
+                return RedirectToAction("Page404", "Home");
+            }
+            return View(vm);
         }
     }
 }
